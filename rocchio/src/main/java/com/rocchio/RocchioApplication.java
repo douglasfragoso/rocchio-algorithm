8f29f28420f14f41dba375d4651396b3787ef578
Douglas Fragoso <douglas.iff@gmail.com>
2025-12-04 20:39:42 -0300

    docs(spring): spring initializr

---
+++ rocchio/src/main/java/com/rocchio/RocchioApplication.java
@@ -0,0 +1,13 @@
+package com.rocchio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocchioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocchioApplication.class, args);
+	}
+
+}
---
+++ rocchio/src/test/java/com/rocchio/RocchioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocchio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocchioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
